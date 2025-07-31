da4ca8c717fbf1bdf127f008c0642a669b55a84c
nothazel24 <frdnryann@gmail.com>
2025-07-31 22:43:28 +0700

    change navbar background when scrolling

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,68 +1,71 @@
 <!-- NAVBAR SECTION -->
-<nav class="container navbar navbar-expand-lg bg-transparent py-3 d-flex flex-row fixed-top">
+<nav class="container-fluid navbar navbar-expand-lg py-3 d-flex flex-row fixed-top">
 
-    <a class="navbar-brand" href="/">
-        <img src="{{ asset('dist/logo/qnoite_logo.png') }}" alt="qnoite" width="110">
-    </a>
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="{{ asset('dist/logo/qnoite_logo.png') }}" alt="qnoite" width="110">
+        </a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
 
-    <div class="collapse navbar-collapse justify-content-end">
-        <ul class="navbar-nav">
+        <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
 
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="/">Home</a>
-            </li>
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="/profile">Profile</a>
-            </li>
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="/price">Paket & harga</a>
-            </li>
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="#">Kontak</a>
-            </li>
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="#">Blog</a>
-            </li>
-            <li class="nav-item active me-3">
-                <a class="nav-link text-white @yield('homeActive')" href="#">Open source</a>
-            </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('/')) active-link @endif" href="/">Home</a>
+                </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('profile')) active-link @endif" href="/profile">Profile</a>
+                </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('price')) active-link @endif" href="/price">Paket & harga</a>
+                </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('#')) active-link @endif" href="#">Kontak</a>
+                </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('#')) active-link @endif" href="#">Blog</a>
+                </li>
+                <li class="nav-item active me-3">
+                    <a class="nav-link @if(request()->is('#')) active-link @endif" href="#">Open source</a>
+                </li>
 
-            @guest
-                @if (Route::has('login'))
-                    <li class="nav-item">
-                        <a class="nav-link fw-bold text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle fw-bold text-white" href="#"
-                        role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
-                            {{ __('Dashboard') }}
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link fw-bold" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle fw-bold" href="#"
+                            role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
                         </a>
 
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                                {{ __('Dashboard') }}
                             </a>
-                        </form>
-                    </div>
-                </li>
-            @endguest
 
-        </ul>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
+                        </div>
+                    </li>
+                @endguest
 
+            </ul>
+
+        </div>
     </div>
+
 </nav> <!-- END NAV SECTION -->
