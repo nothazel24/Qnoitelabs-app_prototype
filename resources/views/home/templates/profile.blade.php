7c808e4c5fa0caec379baf052db4b3734d752819
nothazel24 <frdnryann@gmail.com>
2025-07-31 22:40:47 +0700

    responsible design

--- resources/views/home/templates/profile.blade.php
+++ resources/views/home/templates/profile.blade.php
@@ -1,15 +1,22 @@
 <!-- COMPANY PROFILE -->
 <section class="profile" style="background-color: #81C3D7">
-    <div class="container d-flex justify-between align-items-center gap-5 py-5">
-        <img src="{{ asset('dist/images/qnoite.png') }}" alt="COMPANY" width="400" class="rounded">
-        <div class="d-flex flex-column">
-            <h1 style="font-size: 30px;">Qnoite Projects</h1>
-            <p>Qnoite adalah penyedia layanan pembuatan website profesional yang didirikan pada tahun 2025. Qnoite
-                mengutamakan pengembangan website yang berkualitas, kecepatan website, dan desain modern.Kami
-                membantu bisnis dan individu membangun kehadiran digital yang kuat, sekaligus mendukung pengembangan
-                teknologi melalui proyek eksperimental seperti Qnoitelabs.</p>
-            <a href="/" class="bg-transparent w-25 text-center rounded py-2"
-                style="text-decoration: none; border: 1px solid #252525; color: #252525;"> Profile Kami </a>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-5">
+                <img src="{{ asset('dist/images/qnoite.png') }}" alt="COMPANY" width="400" class="rounded">
+            </div>
+
+            <div class="col-lg-7">
+            <div class="d-flex flex-column">
+                <h1 style="font-size: 30px;">Qnoite Projects</h1>
+                <p>Qnoite adalah penyedia layanan pembuatan website profesional yang didirikan pada tahun 2025. Qnoite
+                    mengutamakan pengembangan website yang berkualitas, kecepatan website, dan desain modern.Kami
+                    membantu bisnis dan individu membangun kehadiran digital yang kuat, sekaligus mendukung pengembangan
+                    teknologi melalui proyek eksperimental seperti Qnoitelabs.</p>
+                <a href="/" class="bg-transparent w-25 text-center rounded py-2"
+                    style="text-decoration: none; border: 1px solid #252525; color: #252525;"> Profile Kami </a>
+            </div>
+        </div>
         </div>
     </div>
 </section>
