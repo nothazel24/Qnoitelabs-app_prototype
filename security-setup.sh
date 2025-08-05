1cf818878908c03e4371905396f6384f36d90e6e
nothazel24 <frdnryann@gmail.com>
2025-08-05 12:42:19 +0700

    For fedora user/linux with SELinux enabled

---
+++ security-setup.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Ini digunakan untuk pengguna fedora yang dimana SELinux nya itu sangat ketat. 
+# Eksekusi kembali kode ini apabila ada gambar/file yang baru ditambahkan kembali dan tidak terdeteksi oleh browser
+
+# sedikit saran, matikan SELinux untuk sementara dalam pengembangan website dengan menggunakan fedora os
+
+sudo chcon -Rt svirt_sandbox_file_t .
+sudo chcon -Rt svirt_sandbox_file_t public
+sudo chcon -Rt svirt_sandbox_file_t storage
+
+echo "Proses selesai..."
