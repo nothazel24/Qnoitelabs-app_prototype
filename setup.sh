ce8bc9237c3b08e7c43a30345bf3507da5937933
nothazel24 <frdnryann@gmail.com>
2025-08-07 21:53:38 +0700

    fixed

--- setup.sh
+++ setup.sh
@@ -3,22 +3,26 @@
 # Setup Project
 
 # Build image
-sudo docker-compose build
+sudo docker compose up -d --build
 
 # Konfigurasi
-sudo docker-compose exec app composer install
-sudo docker-compose exec app php artisan key:generate
+sudo docker compose exec app composer install
+sudo docker compose exec app php artisan key:generate
 
-sudo docker-compose exec app php artisan migrate
+sudo docker compose exec app php artisan migrate
+
+# Tambahkan command dibawah jika kamu ingin memasukan seedernya juga
+# sudo docker compose exec app php artisan migrate --seed
 
 # Link ke storage
-sudo docker-compose exec app php artisan storage:link
+sudo docker compose exec app php artisan storage:link
 
 # NPM install
 # Kamu bisa mengeksekusi kode dibawah ini kembali jika kamu memodifikasi package yang dibutuhkan
 # Untuk clean installation, kamu perlu menghapus file node_modules & package-lock.json yang lama terlebih dahulu
 # Jika sudah, kamu bisa mengeksekusi kode dibawah untuk memperbarui file node_modules mu
 
-sudo docker-compose exec npm npm install
+sudo docker compose exec npm npm install
 
-echo "Proses Selesai..."
+# Start npm 
+sudo docker compose exec npm npm run dev
